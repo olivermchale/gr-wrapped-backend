6bcdc72fd8bcfcc632fb0f2f711ec47d83a20e9c
Oliver McHale <s6012976@live.tees.ac.uk>
2022-12-23 17:02:38 +0000

    Handle no books being found

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,10 @@ export class AppController {
   @Post()
   async ScrapeUrl(@Body() body): Promise<string[]> {
     const profileUrl = body.profileUrl;
-    return await this.appService.scrapeGoodreadsProfile(profileUrl);
+    const books = await this.appService.scrapeGoodreadsProfile(profileUrl);
+    if (books.length === 0) {
+      throw new Error('Failed to load goodreads data');
+    }
+    return books;
   }
 }
