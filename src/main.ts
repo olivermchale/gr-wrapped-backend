7e60b83f4e302c31bfea305158a5c37698688eb3
Oliver McHale <s6012976@live.tees.ac.uk>
2022-12-23 16:24:25 +0000

    Adds deployed front end CORS config

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:4200'] });
+  app.enableCors({
+    origin: ['http://localhost:4200', 'https://goodreads-wrapped.netlify.app/'],
+  });
   await app.listen(process.env.PORT || 3000);
   console.log('Server started on: ', process.env.PORT || 3000);
 }
