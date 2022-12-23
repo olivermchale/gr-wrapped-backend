9e71a2d0abb250467ca26c3f9326c7d963cdd570
Oliver McHale <s6012976@live.tees.ac.uk>
2022-12-23 15:10:30 +0000

    debug log

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:4200'] });
   await app.listen(process.env.PORT || 3000);
+  console.log('Server started on: ', process.env.PORT || 3000);
 }
 bootstrap();
